e81cbbda20c0b32f80613931f889206772c341d8
github username <purnima.nyc@gmail.com>
2018-11-06 14:58:55 -0500

    update - create action

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -11,7 +11,7 @@ class Api::EmployeesController < ApplicationController
     if @employee.save
       render json: @employee
     else
-      render json: ( errors:  (message: "This object failed to save"))
+      render json: { errors:  {message: "This object failed to save"}}
 
     end 
   end 
