f576780342e1bdda159c6d87d98556b91afa639d
github username <purnima.nyc@gmail.com>
2018-11-06 14:41:37 -0500

    add - create action and employee params

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -5,5 +5,20 @@ class Api::EmployeesController < ApplicationController
     render json: @employees
   end 
 
+  def create
+    @employee = Employee.new(employee_params)
 
+    if @employee.save
+      render json: @employee
+    else
+      render json: ( errors:  (message: "This object failed to save"))
+
+    end 
+  end 
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :title, :department, :location)
+  end 
 end 
