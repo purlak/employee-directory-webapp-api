3bb46a7af2c869905d2a7dec534d09c144c95bb4
github username <purnima.nyc@gmail.com>
2018-11-07 11:14:11 -0500

    add - attribute validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :title, presence:true
+  validates :department, presence:true
+  validates :location, presence:true
 end
