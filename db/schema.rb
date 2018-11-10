cfda3c34b2473faf6b9c86bd2c32adf3746cbda7
github username <purnima.nyc@gmail.com>
2018-11-10 14:08:12 -0500

    add - password column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_185239) do
+ActiveRecord::Schema.define(version: 2018_11_10_190718) do
 
   create_table "employees", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_185239) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.string "password_digest"
   end
 
 end
